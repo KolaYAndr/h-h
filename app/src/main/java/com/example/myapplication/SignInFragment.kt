de9f745e93fb546ab3e112e54862df105097f801
R1505-We2-10-Stud <R1505-We2-10-Stud@ad.itmo.ru>
2023-11-03 17:47:23 +0300

    s drugogo kompa

---
+++ app/src/main/java/com/example/myapplication/SignInFragment.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class SignInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = requireView().findViewById<Button>(R.id.signInButton)
+
+        button.setOnClickListener {
+            Log.d("check", "works")
+        }
+    }
+}
