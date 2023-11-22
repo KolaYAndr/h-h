14fcd28c464159d7b78df105b7f1914df74298e3
KolaYAndr <nikolay_andreev02@mail.ru>
2023-11-22 12:03:07 +0300

    vse

---
+++ Lesson_8_Andreev/app/src/main/java/com/example/myapplication/domen/usecase/ProductUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.domen.usecase
+
+import com.example.myapplication.data.product.Product
+import com.example.myapplication.data.repository.AccessRepository
+import javax.inject.Inject
+
+class ProductUseCase @Inject constructor(
+    private val repository: AccessRepository,
+) {
+    suspend fun getProducts(pageSize: Int) : List<Product> {
+        return repository.getProducts(pageSize)
+    }
+}
