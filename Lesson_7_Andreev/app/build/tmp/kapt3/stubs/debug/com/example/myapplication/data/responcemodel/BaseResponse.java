2b17898ea3db8d4323efcd205d61467e73fcc442
KolaYAndr <nikolay_andreev02@mail.ru>
2023-11-22 12:04:51 +0300

    vse

---
+++ Lesson_7_Andreev/app/build/tmp/kapt3/stubs/debug/com/example/myapplication/data/responcemodel/BaseResponse.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.data.responcemodel;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u000e\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0000\n\u0002\b\u0006\u0018\u0000*\u0004\b\u0000\u0010\u00012\u00020\u0002B\r\u0012\u0006\u0010\u0003\u001a\u00028\u0000\u00a2\u0006\u0002\u0010\u0004R\u0018\u0010\u0003\u001a\u00028\u00008\u0006X\u0087\u0004\u00a2\u0006\n\n\u0002\u0010\u0007\u001a\u0004\b\u0005\u0010\u0006\u00a8\u0006\b"}, d2 = {"Lcom/example/myapplication/data/responcemodel/BaseResponse;", "T", "", "data", "(Ljava/lang/Object;)V", "getData", "()Ljava/lang/Object;", "Ljava/lang/Object;", "app_debug"})
+public final class BaseResponse<T extends java.lang.Object> {
+    @com.google.gson.annotations.SerializedName(value = "data")
+    private final T data = null;
+    
+    public BaseResponse(T data) {
+        super();
+    }
+    
+    public final T getData() {
+        return null;
+    }
+}
---
+++ Lesson_7_Andreev/app/build/tmp/kapt3/stubs/debug/com/example/myapplication/presentation/ui/fragments/catalog_fragment/CatalogFragment.java
@@ -0,0 +1,63 @@
+package com.example.myapplication.presentation.ui.fragments.catalog_fragment;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000R\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0016\u001a\u00020\u0017H\u0002J\u0010\u0010\u0018\u001a\u00020\u00172\u0006\u0010\u0019\u001a\u00020\u001aH\u0016J$\u0010\u001b\u001a\u00020\u001c2\u0006\u0010\u001d\u001a\u00020\u001e2\b\u0010\u001f\u001a\u0004\u0018\u00010 2\b\u0010!\u001a\u0004\u0018\u00010\"H\u0016J\b\u0010#\u001a\u00020\u0017H\u0016J\u001a\u0010$\u001a\u00020\u00172\u0006\u0010%\u001a\u00020\u001c2\b\u0010!\u001a\u0004\u0018\u00010\"H\u0016J\u0010\u0010&\u001a\u00020\u00172\u0006\u0010%\u001a\u00020\u001cH\u0002R\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0005\u001a\u00020\u00048BX\u0082\u0004\u00a2\u0006\u0006\u001a\u0004\b\u0006\u0010\u0007R\u000e\u0010\b\u001a\u00020\tX\u0082.\u00a2\u0006\u0002\n\u0000R\u001b\u0010\n\u001a\u00020\u000b8BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u000e\u0010\u000f\u001a\u0004\b\f\u0010\rR\u001e\u0010\u0010\u001a\u00020\u00118\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0012\u0010\u0013\"\u0004\b\u0014\u0010\u0015\u00a8\u0006\'"}, d2 = {"Lcom/example/myapplication/presentation/ui/fragments/catalog_fragment/CatalogFragment;", "Landroidx/fragment/app/Fragment;", "()V", "_binding", "Lcom/example/myapplication/databinding/FragmentCatalogBinding;", "binding", "getBinding", "()Lcom/example/myapplication/databinding/FragmentCatalogBinding;", "catalogAdapter", "Lcom/example/myapplication/presentation/ui/fragments/catalog_fragment/CatalogAdapter;", "catalogViewModel", "Lcom/example/myapplication/presentation/ui/fragments/catalog_fragment/CatalogViewModel;", "getCatalogViewModel", "()Lcom/example/myapplication/presentation/ui/fragments/catalog_fragment/CatalogViewModel;", "catalogViewModel$delegate", "Lkotlin/Lazy;", "viewModelFactory", "Landroidx/lifecycle/ViewModelProvider$Factory;", "getViewModelFactory", "()Landroidx/lifecycle/ViewModelProvider$Factory;", "setViewModelFactory", "(Landroidx/lifecycle/ViewModelProvider$Factory;)V", "initAdapter", "", "onAttach", "context", "Landroid/content/Context;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onDestroyView", "onViewCreated", "view", "setCatalogViewModelObserver", "app_debug"})
+public final class CatalogFragment extends androidx.fragment.app.Fragment {
+    @org.jetbrains.annotations.Nullable
+    private com.example.myapplication.databinding.FragmentCatalogBinding _binding;
+    private com.example.myapplication.presentation.ui.fragments.catalog_fragment.CatalogAdapter catalogAdapter;
+    @org.jetbrains.annotations.NotNull
+    private final kotlin.Lazy catalogViewModel$delegate = null;
+    @javax.inject.Inject
+    public androidx.lifecycle.ViewModelProvider.Factory viewModelFactory;
+    
+    public CatalogFragment() {
+        super();
+    }
+    
+    private final com.example.myapplication.databinding.FragmentCatalogBinding getBinding() {
+        return null;
+    }
+    
+    private final com.example.myapplication.presentation.ui.fragments.catalog_fragment.CatalogViewModel getCatalogViewModel() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final androidx.lifecycle.ViewModelProvider.Factory getViewModelFactory() {
+        return null;
+    }
+    
+    public final void setViewModelFactory(@org.jetbrains.annotations.NotNull
+    androidx.lifecycle.ViewModelProvider.Factory p0) {
+    }
+    
+    @java.lang.Override
+    public void onAttach(@org.jetbrains.annotations.NotNull
+    android.content.Context context) {
+    }
+    
+    @java.lang.Override
+    @org.jetbrains.annotations.NotNull
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    @java.lang.Override
+    public void onViewCreated(@org.jetbrains.annotations.NotNull
+    android.view.View view, @org.jetbrains.annotations.Nullable
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    @java.lang.Override
+    public void onDestroyView() {
+    }
+    
+    private final void setCatalogViewModelObserver(android.view.View view) {
+    }
+    
+    private final void initAdapter() {
+    }
+}
