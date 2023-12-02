ca4451c13279b52785d0cbc1efa62baafc9f4b17
KolaYAndr <nikolay_andreev02@mail.ru>
2023-12-02 18:27:48 +0300

    Full ui, created necessary classes

---
+++ Lesson_8_Andreev/app/src/main/java/com/example/myapplication/presentation/ui/fragments/bottom_sheet_fragment/BottomSheetAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.presentation.ui.fragments.bottom_sheet_fragment
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BottomSheetAdapter : RecyclerView.Adapter<BottomSheetAdapter.BottomSheetViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BottomSheetViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BottomSheetViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    inner class BottomSheetViewHolder(view: View) : RecyclerView.ViewHolder(view)
+}
---
+++ Lesson_8_Andreev/app/src/main/java/com/example/myapplication/presentation/ui/fragments/bottom_sheet_fragment/BottomSheetFragment.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.presentation.ui.fragments.bottom_sheet_fragment
+
+import androidx.fragment.app.Fragment
+
+class BottomSheetFragment : Fragment() {
+}
